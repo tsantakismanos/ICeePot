6c76f2ae6b3191666fdda42e613427f0bf38dafc
tsantakismanos <tsantakismanos@gmail.com>
2013-02-27 12:08:08 +0200

    settings xml creation if non-existent in the beginning

--- software/ICeePotPC/src/iceepotpc/appication/Context.java
+++ software/ICeePotPC/src/iceepotpc/appication/Context.java
@@ -1,13 +1,20 @@
 package iceepotpc.appication;
 
 
+
 import java.io.File;
+
+import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.Observer;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
@@ -16,6 +23,8 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+
+
 
 /**
  * @author tsantakis
@@ -60,60 +69,17 @@ public class Context{
 	 */
 	private Context() {
 		
-		uiElements = new ArrayList<Observer>();
-		
-		Document dom;
-		
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		
-		try {
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			
-			dom = db.parse("Settings.xml");
-			
-			
-			//get the server name
-			try{
-				NodeList nlstServer = dom.getElementsByTagName("server_hostname");
-				serverHost = nlstServer.item(0).getTextContent();
-			}catch(Exception e){
-				serverHost = "N/A";
-			}
-					
-		
-			//get the server port
-			try{
-				NodeList nlstServer = dom.getElementsByTagName("server_port");
-				serverPort = Integer.parseInt(nlstServer.item(0).getTextContent());
-			}catch(Exception e){
-				serverPort = -1;
-			}
-			
+		File f = new File("settings.xml");
+		if(!f.exists())
 			try {
-				//get the list of pots
-				NodeList nlstPots = dom.getElementsByTagName("pot");
-				
-				//getting each pot's details
-				for(int i=0; i<nlstPots.getLength(); i++){
-					
-					//NodeList nlstPotDetails = nlstPots.item(i).getChildNodes();
-					
-					String s = (((Element)nlstPots.item(i)).getElementsByTagName("descr")).item(0).getTextContent();
-					int j = Integer.parseInt((((Element)nlstPots.item(i)).getElementsByTagName("pin")).item(0).getTextContent());
-					
-					Pot p = new Pot(s,j);
-					
-					potDescrs.add(p);
-				}
-			} catch (Exception e){
-				potDescrs = null;
+				createSettings();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			
-		} catch (Exception e){
-			serverHost = "N/A";
-			serverPort = -1;
-			potDescrs = null;
-		}
+		
+		
+		fetchSettings();
 	}
 	
 	
@@ -240,6 +206,115 @@ public class Context{
 		} catch (Exception e){
 			throw e;
 		}
+	}
+	
+	/**
+	 * helper private method called by the constructor to fetch the 
+	 * settings from settings.xml file & store it to the context variables
+	 */
+	private void fetchSettings(){
+		uiElements = new ArrayList<Observer>();
+		
+		Document dom;
+		
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		
+		try {
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			
+			dom = db.parse("Settings.xml");
+			
+			
+			//get the server name
+			try{
+				NodeList nlstServer = dom.getElementsByTagName("server_hostname");
+				serverHost = nlstServer.item(0).getTextContent();
+			}catch(Exception e){
+				serverHost = "N/A";
+			}
+					
+		
+			//get the server port
+			try{
+				NodeList nlstServer = dom.getElementsByTagName("server_port");
+				serverPort = Integer.parseInt(nlstServer.item(0).getTextContent());
+			}catch(Exception e){
+				serverPort = -1;
+			}
+			
+			try {
+				//get the list of pots
+				NodeList nlstPots = dom.getElementsByTagName("pot");
+				
+				//getting each pot's details
+				for(int i=0; i<nlstPots.getLength(); i++){
+					
+					//NodeList nlstPotDetails = nlstPots.item(i).getChildNodes();
+					
+					String s = (((Element)nlstPots.item(i)).getElementsByTagName("descr")).item(0).getTextContent();
+					int j = Integer.parseInt((((Element)nlstPots.item(i)).getElementsByTagName("pin")).item(0).getTextContent());
+					
+					Pot p = new Pot(s,j);
+					
+					potDescrs.add(p);
+				}
+			} catch (Exception e){
+				potDescrs = null;
+			}
+			
+		} catch (Exception e){
+			serverHost = "N/A";
+			serverPort = -1;
+			potDescrs = null;
+		}
+	}
+	
+	
+	/**
+	 * helper method that creates the settings.xml file
+	 * @throws IOException 
+	 */
+	private void createSettings() throws IOException{
+		
+		Document dom;
+		
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+				
+		try {
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			
+			dom = db.newDocument();
+			
+			Element rootElement = dom.createElement("settings");
+			dom.appendChild(rootElement);
+			
+			Element srvNameElement = dom.createElement("server_hostname");
+			srvNameElement.appendChild(dom.createTextNode(""));
+			rootElement.appendChild(srvNameElement);
+			
+			Element srvPortElement = dom.createElement("server_port");
+			srvPortElement.appendChild(dom.createTextNode(""));
+			rootElement.appendChild(srvPortElement);
+			
+			
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(dom);
+			StreamResult result = new StreamResult(new File("settings.xml"));
+			transformer.transform(source, result);
+			
+			
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  catch (TransformerConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
 	}
 	
 	public void registerObserver(Observer obs){
