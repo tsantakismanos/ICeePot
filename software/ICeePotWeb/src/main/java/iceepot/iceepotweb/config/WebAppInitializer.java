80d70ad8d0c49b4a5b47e1990a223d2a32271f38
tsantakismanos <tsantakismanos@gmail.com>
2016-02-17 23:51:51 +0200

    first mvc configuration

---
+++ software/ICeePotWeb/src/main/java/iceepot/iceepotweb/config/WebAppInitializer.java
@@ -0,0 +1,26 @@
+package iceepot.iceepotweb.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends
+		AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		
+		return new Class<?>[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		
+		return new Class<?>[] {WebConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		
+		return new String[]{"/"};
+	}
+
+}
