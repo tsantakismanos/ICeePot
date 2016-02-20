1057af34fb8d29ee8798cc43572fc3c11bf0b521
tsantakismanos <tsantakismanos@gmail.com>
2016-02-20 19:38:00 +0200

    Created and Tested REST mock controller

--- software/ICeePotWeb/src/test/java/iceepot/iceepotweb/web/PotsControllerTest.java
+++ software/ICeePotWeb/src/test/java/iceepot/iceepotweb/web/PotsControllerTest.java
@@ -1,20 +1,73 @@
 package iceepot.iceepotweb.web;
 
+import iceepot.iceepotweb.config.WebConfig;
+
+import java.util.Date;
+
 import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+
+@ContextConfiguration(classes={WebConfig.class})
 public class PotsControllerTest {
 
 	@Test
 	public void testPotsController() throws Exception{
 		
+		performPotTestUnit(new PotTestUnit(0, (new Date()).getTime() , (new Date()).getTime()));
+		performPotTestUnit(new PotTestUnit(0, (new Date()).getTime() , (new Date()).getTime()));
+		performPotTestUnit(new PotTestUnit(0, (new Date()).getTime() , (new Date()).getTime()));
+		performPotTestUnit(new PotTestUnit(0, (new Date()).getTime() , (new Date()).getTime()));
+	}
+	
+	
+	private void performPotTestUnit(PotTestUnit potTestUnit) throws Exception{
 		PotsController potsController = new PotsController();
 		
 		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(potsController).build(); 
-				
-		mockMvc.perform(MockMvcRequestBuilders.get("/pots/0"));
+		
+		mockMvc.perform(MockMvcRequestBuilders
+						.get("/pots/" + String.valueOf(potTestUnit.getPotId()))
+						.param("timeFrom", String.valueOf(potTestUnit.getTimeFrom()))
+						.param("timeTo",String.valueOf(potTestUnit.getTimeTo())))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8));
+	}
+	
+	
+	
+	private class PotTestUnit{
+		
+		private int potId;
+		private long timeFrom;
+		private long timeTo;
+		
+		public PotTestUnit(int potId, long timeFrom, long timeTo) {
+		
+			this.potId = potId;
+			this.timeFrom = timeFrom;
+			this.timeTo = timeTo;
+		}
+
+		public int getPotId() {
+			return potId;
+		}
+
+		public long getTimeFrom() {
+			return timeFrom;
+		}
+
+		public long getTimeTo() {
+			return timeTo;
+		}
+		
+		
+		
 	}
 	
 }
